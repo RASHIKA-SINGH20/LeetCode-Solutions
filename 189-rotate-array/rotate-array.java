18110d6e7a668d60e53e8a8d83d238fb26d481d0
RASHIKA-SINGH20 <rashikasingh.ishu@outlook.com>
2026-08-26 01:53:14 +0530

    Time: 4 ms (17.42%) | Memory: 268.6 MB (9.71%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,10 +1,14 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k = k % n;
+        k = k % n;   // agar k greater than length ho gya toh woh sare syeps repeat nhi honge
+        if(k < 0)
+        {
+            k+=nums.length;
+        }  
         reverse(nums , 0 , n -1);  // phele poora reverse kr denge
         reverse(nums , 0 , k - 1); //first k element
-        reverse(nums , k , n - 1); // last k element
+        reverse(nums , k , n - 1); // remaining
     }
     public void reverse(int[] nums , int left , int right)
     {
