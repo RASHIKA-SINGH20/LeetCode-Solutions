f8300658ff4ed7f5725c17cb20ac3cadc84bce13
RASHIKA-SINGH20 <rashikasingh.ishu@outlook.com>
2026-09-14 09:35:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (47.38%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,17 +1,23 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] merge = new int[m + n];
-        for(int i = 0 ; i < m ; i++)
+        int i = m - 1;
+        int j = n - 1;
+        int k = (m + n) - 1;
+
+        while(j >= 0)
         {
-            merge[i] = nums1[i];
-        }
-        for(int j = 0 ; j < n ; j++)
-        {
-            merge[m + j] = nums2[j];
-        }
-        Arrays.sort(merge);
-        for (int i = 0; i < m + n; i++) {
-            nums1[i] = merge[i];
+            if(i >= 0 && nums1[i] > nums2[j])
+            {
+                nums1[k] = nums1[i];
+                i--;
+                k--;
+            }
+            else
+            {
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            }
         }
     }
 }
