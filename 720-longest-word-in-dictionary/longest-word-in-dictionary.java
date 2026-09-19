3d70a27a8c42a59396cd3878332d722477597187
RASHIKA-SINGH20 <rashikasingh.ishu@outlook.com>
2026-09-19 19:03:25 +0530

    Time: 17 ms (47.95%) | Memory: 46.4 MB (79.76%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.java
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.java
@@ -1,37 +1,25 @@
 class Solution {
     public String longestWord(String[] words) {
+        Arrays.sort(words);
+
         HashSet<String> set = new HashSet<>();
 
-        for(String word : words)
-        {
-            set.add(word);
-        }
         String res = "";
-        for(String word : words)
-        {
-            boolean longest = true;
-            for(int i = 1 ; i < word.length() ; i++)
-            {
-                String prefix = word.substring(0,i); // substring dekh rahe math ho raha ki nhi to previous
 
-                if(!set.contains(prefix))
-                {
-                    longest = false;
-                    break;
-                }
-            }
-            if(longest)
-            {
-                if(word.length() > res.length())
-                {
-                    res = word;
-                }
-                else if(word.length() == res.length() && word.compareTo(res) < 0)
-                {
+        for (String word : words) {
+
+            String prefix = word.substring(0, word.length() - 1);
+
+            if (word.length() == 1 || set.contains(prefix)) {
+
+                set.add(word);
+
+                if (word.length() > res.length()) {
                     res = word;
                 }
             }
         }
+
         return res;
     }
 }
